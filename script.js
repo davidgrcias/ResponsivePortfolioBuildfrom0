1c7228772e848cc03711256b0527123f83c84d22
davidsproject <davidgarciasaragih6@gmail.com>
2021-05-13 13:01:20 +0700

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.page-scroll').on('click', function(e){
+  var tujuan = $(this).attr('href');
+  var elemenTujuan = $(tujuan);
+  $('html, body').animate({
+    scrollTop: elemenTujuan.offset().top-50}, 1250, 'swing'
+  );
+
+  e.preventDefault();
+});
